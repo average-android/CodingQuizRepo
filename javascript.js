78247d6d271e04e5a33b6f9d5837ef4f4cda69b6
Mitchel Busnel <staysaucy555@gmail.com>
2022-01-18 20:10:45 -0500

    Intial commit with basic file structure

---
+++ javascript.js
@@ -0,0 +1,35 @@
+startBtn.addEventListener("click", QuestionTime);
+
+var score = 0
+
+
+var questionArray = [
+    {
+        question : "Does this work?",
+        a: "No",
+        b: "No",
+        c: "Maybe",
+        d: "Yes",
+        correct: "b"
+    },
+    
+    {
+        question : "Does this also work?",
+        a: "Probably not",
+        b: "Maybe",
+        c: "If you did more work, yeah.",
+        d: "No.",
+        correct: "d"
+    }
+]
+
+
+
+
+
+function QuestionTime() {
+    Countdown()
+
+
+}
+
